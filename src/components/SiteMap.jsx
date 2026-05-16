66e0ef1a02d1590ef8c05c5b88f5697b57b76896
Emre <emre.sahiner@workintech.com.tr>
2026-05-16 10:37:10 +0300

    feat: add SiteMap component

---
+++ src/components/SiteMap.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Container = styled.footer`
+  height: 20vh;
+  background: #252525;
+  color: white;
+`;
+
+function SiteMap() {
+  return <Container>SiteMap</Container>;
+}
+
+export default SiteMap;

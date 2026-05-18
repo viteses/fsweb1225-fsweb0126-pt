b678226af554d6ce17beb08db41f212099ac5209
Emre <emre.sahiner@workintech.com.tr>
2026-05-18 21:28:56 +0300

    feat: add conditional formatting for profile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,13 @@
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
 import styled from "styled-components";
+import blue from "../assets/avatars/blue.png";
 
 const Container = styled.header`
   height: 5rem;
-  border-bottom: 1.5px solid rgba(255, 255, 255, 0.2);
+  ${(props) =>
+    props.profile
+      ? null
+      : "border-bottom: 1.5px solid rgba(255, 255, 255, 0.2);"}
   padding: 0 4rem;
   display: flex;
   align-items: center;
@@ -10,13 +15,64 @@ const Container = styled.header`
 
 const Logo = styled.h1`
   color: red;
-  margin: 0;
+  margin: 0 2rem;
 `;
 
-function Header() {
+const NavMenu = styled.nav`
+  width: 100%;
+  display: flex;
+  gap: 1.2rem;
+  & a {
+    color: white;
+    text-decoration: none;
+  }
+
+  & .active {
+    font-weight: bold;
+  }
+`;
+
+const UserControl = styled.div`
+  color: white;
+  display: flex;
+  gap: 2rem;
+  align-items: center;
+  & a {
+    color: white;
+    text-decoration: none;
+  }
+`;
+
+const ProfileImage = styled.img`
+  border-radius: 0.5rem;
+  width: 3rem;
+`;
+
+function Header(props) {
+  const { className, profile } = props;
+
   return (
-    <Container>
+    <Container className={className} profile={profile}>
       <Logo>WitFLiX</Logo>
+      {profile && (
+        <>
+          <NavMenu>
+            <NavLink to="/browse">Home</NavLink>
+            <NavLink to="/shows">Shows</NavLink>
+            <NavLink to="/movies">Movies</NavLink>
+            <NavLink to="/games">Games</NavLink>
+            <NavLink to="/new">New&Popular</NavLink>
+            <NavLink to="/list">My List</NavLink>
+            <NavLink to="/lang">Browse by Languages</NavLink>
+          </NavMenu>
+          <UserControl>
+            <i class="fa fa-search fa-lg" aria-hidden="true"></i>
+            <NavLink to="/kids">Kids</NavLink>
+            <i class="fa fa-bell fa-lg" aria-hidden="true"></i>
+            <ProfileImage src={blue} />
+          </UserControl>
+        </>
+      )}
     </Container>
   );
 }
